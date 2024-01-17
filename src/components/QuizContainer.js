c3fafc14e16a89e4d2dd0b1cbd20df756dfe7af7
Ragini <raginiv743@gmail.com>
2024-01-17 11:29:55 +0530

    add icrement and decrement

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -1,6 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { data } from '../utils/data'
 const QuizContainer = () => {
+    const [index,setIndex] = useState(0)
+    const [quiz,setQuiz] = useState(data[index])
+    console.log(quiz)
+    // console.log(index)
+    if(index===10){
+        alert("completed")
+        setIndex(0)
+        setQuiz(data[0])
+    }
+if(index<0|| index === undefined){
+     
+    setIndex(0)
+    
+    setQuiz(data[0])
+}
   return (
     <div className=''>
         <div className='bg-gradient-to-tr from-blue-400 via-blue-600
@@ -10,16 +25,35 @@ const QuizContainer = () => {
                 <div><img src='./Frame3.png'alt=''></img></div>
              </div>
              <div className='pt-10 pb-28'>
-                <p className=' text-white font-extrabold text-center text-[2rem]'>9+6+7x-2x-3</p>
+                <p className=' text-white font-extrabold text-center
+                 text-[2rem]'>{quiz?.ques}</p>
              </div>
         </div>
         <div className='md:w-[60%] mx-auto flex justify-evenly py-10 items-center'>
             <div className='w-[3%]'><img src=' ./Vector2.png'alt=''></img></div>
-            <div className='w-[30%]'><img src='./Frame 42.png'alt=''></img></div>
-            {/* <div><img src=''alt=''></img></div> */}
-            <div className='w-[3%]'><img src='./Vector3.png'alt=''></img></div>
+            <div className='w-[35%] flex items-center'>
+                <div> <img src='./Vector (4).png'alt='' onClick={()=>
+            {
+                console.log("index",index)
+                setIndex(index-1) ;
+                console.log(index)
+        setQuiz(data[index-1])
+            }
+            }></img></div>
+               
+                <p className='mx-2'>{index+1}/10</p>
+             
+                <div><img src='./Vector (5).png'alt='' onClick={()=>{
+                    setIndex(1+index) ;
+                    console.log(index)
+            setQuiz(data[index+1])
+                }}></img></div>
+            </div>
+            
+            <div className='w-[3%]'>
+                <img src='./Vector3.png'alt=''></img></div>
         </div>
-        <div className='flex  items-center  justify-between mb-16'>
+        <div className='flex  items-center justify-between mb-16'>
             <div className='w-[15%]'>
                 <img src='./Component 41.png'alt=''></img>
             </div>
---
+++ src/utils/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        ques : "x+y+2"
+    },
+    {
+        ques:"p+q+r"
+    },
+    {
+        ques:"1+p"
+    },
+    {
+        ques:"x+2y+z"
+    },
+     {
+        ques:"x/(2y+3)"
+    }, 
+    {
+        ques:"5+8x"
+    },
+     {
+        ques:"3y+3z"
+    },
+     {
+        ques:"a+b+C"
+    }, 
+    {
+        ques:"a+x+2x+3y"
+    },
+     {
+        ques:"1+1+x"
+    },
+    
+
+]
