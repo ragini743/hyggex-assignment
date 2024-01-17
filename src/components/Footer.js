86842f2a54e1e760d6a7b785452e562e9f8fc408
Ragini <raginiv743@gmail.com>
2024-01-17 20:29:44 +0530

    add accordian

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Footer = () => {
+    const [answer,setAnswer]=useState(false) ;
+    const handleAnswer  = () => {
+       setAnswer(!answer)
+    }
   return (
-    <div className='md:w-[80%] pb-10 w-[80%]'>
+    <div className='md:w-[80%] pb-10 w-[80%] text-xs md:text-sm '>
         <h1 className='font-extrabold text-blue-950 text-[2rem] md:text-[2.5rem] py-4'>FAQ</h1>
         <div className='border-gray-400 border-2 flex 
         justify-between items-center px-2 py-1 rounded-lg mb-4'>
-            <p>Can education flashcards be used for all groups?</p>
-            <div><img src='./Frame11.png'alt=''></img></div>
+            <div> <p>Can education flashcards be used for all groups?</p>
+            {(answer===true)?(<p>losdfghjk
+            </p>):null}</div>
+           
+            <div className='w-[8%]'><img src={(answer===true)?"./collapse.png":'./expand.png'}alt='' className='w-full' onClick={handleAnswer}></img>
+            
+            </div>
+           
         </div>
         <div className='border-gray-400 border-2 flex 
         justify-between items-center px-2 py-1 rounded-lg mb-4'>
-            <p>How do education flashcards work ?</p>
-            <div><img src='./Frame11.png'alt=''></img></div>
+            <div> <p>How do education flashcards work ?</p>
+            {(answer===true)?(<p>losdfghjk
+            </p>):null}</div>
+           
+            <div className='w-[8%]'><img src={(answer===true)?"./collapse.png":'./expand.png'}alt='' className='w-full' onClick={handleAnswer}></img>
+            
+            </div>
         </div>
         <div className='border-gray-400 border-2 flex 
         justify-between items-center px-2 py-1 rounded-lg mb-4'>
-            <p>Can education flashcards be used for test preparation?</p>
-            <div><img src='./Frame11.png'alt=''></img></div>
+           <div> <p>Can education flashcards be used for test preparation?</p>
+            {(answer===true)?(<p>losdfghjk
+            </p>):null}</div>
+            <div className='w-[8%]'><img src={(answer===true)?"./collapse.png":'./expand.png'}alt='' className='w-full' onClick={handleAnswer}></img>
+            
+            </div>
         </div>
 
     </div>
